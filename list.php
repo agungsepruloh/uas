7cbd112fbea8e64e98987f4b3f8c829b56eb2ff4
unknown <fadlullahyazid@gmail.com>
2019-07-28 20:56:37 +0700

    added transaction function

---
+++ list.php
@@ -0,0 +1,76 @@
+<?php
+
+$foodsanddrinks = array(
+    1 => array(
+        'nama' => 'Pizza With Berries',
+        'gambar' => './img/products/pizza1.jpg',
+        'harga' => 100000,
+        'tipe' => 'food'
+    ),
+    2 => array(
+        'nama' => 'Cheese Pepperoni Pizza',
+        'gambar' => './img/products/pizza2.jpg',
+        'harga' => 200000,
+        'tipe' => 'food'
+    ),
+    3 => array(
+        'nama' => 'Classic Pizza',
+        'gambar' => './img/products/pizza3.jpg',
+        'harga' => 300000,
+        'tipe' => 'food'
+    ),
+    4 => array(
+        'nama' => 'Round Pizza',
+        'gambar' => './img/products/pizza4.jpg',
+        'harga' => 400000,
+        'tipe' => 'food'
+    ),
+    5 => array(
+        'nama' => 'Pepperoni Pizza',
+        'gambar' => './img/products/pizza5.jpg',
+        'harga' => 500000,
+        'tipe' => 'food'
+    ),
+    6 => array(
+        'nama' => 'Pizza Box',
+        'gambar' => './img/products/pizza6.jpg',
+        'harga' => 600000,
+        'tipe' => 'food'
+    ),
+    7 => array(
+        'nama' => 'Water and Lemon',
+        'gambar' => './img/products/drink1.jpg',
+        'harga' => 100000,
+        'tipe' => 'drink'
+    ),
+    8 => array(
+        'nama' => 'Lemon Juice',
+        'gambar' => './img/products/drink2.jpg',
+        'harga' => 200000,
+        'tipe' => 'drink'
+    ),
+    9 => array(
+        'nama' => 'Liquor Pouring',
+        'gambar' => './img/products/drink3.jpg',
+        'harga' => 300000,
+        'tipe' => 'drink'
+    ),
+    10 => array(
+        'nama' => 'Lemon and Strawberry Juice',
+        'gambar' => './img/products/drink4.jpg',
+        'harga' => 400000,
+        'tipe' => 'drink'
+    ),
+    11 => array(
+        'nama' => 'Watermelon with Lemon',
+        'gambar' => './img/products/drink5.jpg',
+        'harga' => 500000,
+        'tipe' => 'drink'
+    ),
+    12 => array(
+        'nama' => 'Sliced Citrus Fruit',
+        'gambar' => './img/products/drink6.jpg',
+        'harga' => 600000,
+        'tipe' => 'drink'
+    ),
+);
---
+++ transaksi.php
@@ -0,0 +1,80 @@
+<?php 
+session_start();
+
+if ( !isset($_SESSION['login']) ) {
+    header('Location: login.php');
+    exit;
+}
+
+require('list.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Food Website | Welcome</title>
+        <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="css/second.css">
+        <style>
+            .btn {
+                padding: 5px;
+                min-width: 100px;
+            }
+            .box {
+                float: none!important;
+                display: inline-block;
+            }
+            .product {
+                padding-bottom: 20px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <header>
+            <div class="container">
+                <div id="branding">
+                    <h1><span class="highlight">Food</span> Website</h1>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="services.php">Services</a></li>
+                        <li class="current"><a href="products.php">Products</a></li>
+                        <?php if ( isset($_SESSION['login'])) : ?>
+                            <li><a href="logout.php">Logout</a></li>
+                        <?php endif; ?>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <section id="product">
+            <div class="container product">
+                <h1 class="page-title">Transaction</h1>
+                <?php
+                if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    echo '
+                    <input type="text" name="nama" id="">
+                    <input type="text" name="pesanan" id="" value="'.$foodsanddrinks[$_POST['id']]['nama'].'" readonly>
+                    <input type="text" name="jumlah" id="" value="'.$_POST['jumlah'].'" readonly>
+                    <input type="text" name="harga" id="" value="'.$foodsanddrinks[$_POST['id']]['harga'].'" readonly>
+                    <input type="text" name="totalharga" value="'.$foodsanddrinks[$_POST['id']]['harga'] * $_POST['jumlah'].'" id="" readonly>
+                    ';
+                }
+                ?>
+                
+            </div>
+        </section>
+
+        <footer>
+            <p>Food Website, Copyright &copy; <?= date('Y') ?></p>
+        </footer>
+    </body>
+
+    <script>
+    </script>
+</html>
